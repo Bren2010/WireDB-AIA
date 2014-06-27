f5adf28f67e67a9548cd5d63f5ad2fd3ad6fd3be
Brendan Mc <brendanmcmillion@gmail.com>
2014-06-27 12:44:44 -0400

    Cleaned up tests.

--- test/auth.js
+++ test/auth.js
@@ -5,6 +5,20 @@ var bencode = require('bencode')
 
 var loc = 'http://localhost:' + config.port + '/?chall='
 
+var fetch = function(query, done) {
+    http.get(loc + query, function (res) {
+        var buff = new Buffer(0)
+
+        res.on('data', function(data) {
+            buff = Buffer.concat([buff, data])
+        })
+
+        res.on('end', function() {
+            done(buff)
+        })
+    })
+}
+
 console.log('AIA server must be running!')
 
 describe('Authentication', function() {
@@ -12,26 +26,18 @@ describe('Authentication', function() {
         var chall = 'user1:pass1'
         var attrs = ['a', 'b', 'c']
 
-        http.get(loc + chall, function (res) {
-            var buff = new Buffer(0)
+        fetch(chall, function (res) {
+            try {
+                var obj = bencode.decode(res, 'utf8')
+                var t = obj.attrs.pop()
 
-            res.on('data', function(data) {
-                buff = Buffer.concat([buff, data])
-            })
+                assert.deepEqual(obj.attrs, attrs)
+                assert.equal(t, 'time=' + Math.floor(Date.now() / 1000))
 
-            res.on('end', function() {
-                try {
-                    var obj = bencode.decode(buff, 'utf8')
-                    var t = obj.attrs.pop()
-
-                    assert.deepEqual(obj.attrs, attrs)
-                    assert.equal(t, 'time=' + Math.floor(Date.now() / 1000))
-
-                    done()
-                } catch (err) {
-                    throw 'Was expecting bencode, but got error:  ' + err
-                }
-            })
+                done()
+            } catch (err) {
+                throw 'Was expecting bencode, but got error:  ' + err
+            }
         })
     })
 
@@ -39,64 +45,40 @@ describe('Authentication', function() {
         var chall = 'user2:pass2'
         var attrs = ['b', 'd']
 
-        http.get(loc + chall, function (res) {
-            var buff = new Buffer(0)
+        fetch(chall, function (res) {
+            try {
+                var obj = bencode.decode(res, 'utf8')
+                var t = obj.attrs.pop()
 
-            res.on('data', function(data) {
-                buff = Buffer.concat([buff, data])
-            })
+                assert.deepEqual(obj.attrs, attrs)
+                assert.equal(t, 'time=' + Math.floor(Date.now() / 1000))
 
-            res.on('end', function() {
-                try {
-                    var obj = bencode.decode(buff, 'utf8')
-                    var t = obj.attrs.pop()
-
-                    assert.deepEqual(obj.attrs, attrs)
-                    assert.equal(t, 'time=' + Math.floor(Date.now() / 1000))
-
-                    done()
-                } catch (err) {
-                    throw 'Was expecting bencode, but got error:  ' + err
-                }
-            })
+                done()
+            } catch (err) {
+                throw 'Was expecting bencode, but got error:  ' + err
+            }
         })
     })
 
     it('should fail to authenticate unkown user', function (done) {
         var chall = 'herp:derp'
 
-        http.get(loc + chall, function (res) {
-            var buff = new Buffer(0)
+        fetch(chall, function (res) {
+            var test = res.toString()
 
-            res.on('data', function(data) {
-                buff = Buffer.concat([buff, data])
-            })
+            assert.equal(test, 'Permission denied.')
 
-            res.on('end', function() {
-                var test = buff.toString()
-
-                assert.equal(test, 'Permission denied.')
-
-                done()
-            })
+            done()
         })
     })
 
     it('should fail to authenticate bad syntax', function (done) {
-        http.get(loc, function (res) {
-            var buff = new Buffer(0)
+        fetch('', function (res) {
+            var test = res.toString()
 
-            res.on('data', function(data) {
-                buff = Buffer.concat([buff, data])
-            })
+            assert.equal(test, 'Permission denied.')
 
-            res.on('end', function() {
-                var test = buff.toString()
-
-                assert.equal(test, 'Permission denied.')
-
-                done()
-            })
+            done()
         })
     })
 })
--- test/crypto.js
+++ test/crypto.js
@@ -11,12 +11,8 @@ var sjcl = require('./../sjcl')
 
 var loc = 'http://localhost:' + config.port + '/?chall=user1:pass1'
 
-var c = sjcl.ecc.curves[config.key.curve]
-var bits = sjcl.codec.hex.toBits(config.key.pub)
-var pubKey = new sjcl.ecc.ecdsa.publicKey(c, bits)
-
-it('Verifying cryptography', function (done) {
-    http.get(loc, function (res) {
+var fetch = function(query, done) {
+    http.get(loc + query, function (res) {
         var buff = new Buffer(0)
 
         res.on('data', function(data) {
@@ -24,24 +20,34 @@ it('Verifying cryptography', function (done) {
         })
 
         res.on('end', function() {
-            var obj = bencode.decode(buff, 'utf8')
-
-            var c = Math.pow(2, Math.ceil(Math.log(obj.attrs.length) / Math.log(2)))
-            c = c - obj.attrs.length
-
-            if (c === 0) { obj.attrs.push('0000000000') }
-
-            var tree = new caesar.tree.Committer(obj.attrs, 'sha1')
-            var signer = new caesar.kts.Signer(1, obj.seed)
-
-            tree.vals[tree.vals.length - 1] = signer.getPublicKey()
-
-            var head = sjcl.codec.hex.toBits(tree.getCommit())
-            var sig = sjcl.codec.base64.toBits(obj.sig)
-
-            pubKey.verify(head, sig)
-
-            done()
+            done(buff)
         })
+    })
+}
+
+var c = sjcl.ecc.curves[config.key.curve]
+var bits = sjcl.codec.hex.toBits(config.key.pub)
+var pubKey = new sjcl.ecc.ecdsa.publicKey(c, bits)
+
+it('Verifying cryptography', function (done) {
+    fetch('', function (res) {
+        var obj = bencode.decode(res, 'utf8')
+
+        var c = Math.pow(2, Math.ceil(Math.log(obj.attrs.length) / Math.log(2)))
+        c = c - obj.attrs.length
+
+        if (c === 0) { obj.attrs.push('0000000000') }
+
+        var tree = new caesar.tree.Committer(obj.attrs, 'sha1')
+        var signer = new caesar.kts.Signer(1, obj.seed)
+
+        tree.vals[tree.vals.length - 1] = signer.getPublicKey()
+
+        var head = sjcl.codec.hex.toBits(tree.getCommit())
+        var sig = sjcl.codec.base64.toBits(obj.sig)
+
+        pubKey.verify(head, sig)
+
+        done()
     })
 })
