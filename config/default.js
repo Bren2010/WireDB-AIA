6fecba530da23820cbac6b5d730e29cb1c3f3bca
Brendan Mc. <brendanmcmillion@gmail.com>
2014-06-29 12:57:28 -0400

    Update default.js

--- config/default.js
+++ config/default.js
@@ -3,7 +3,7 @@ path = require('path')
 module.exports.port     = 3001
 module.exports.database = 'memory'
 
-module.exports.key = {
+module.exports.key = { // The AIA's master signing key.  (curve, pubKey, secKey)
     curve: 'c256',
     pub: '07d736dc6247d1b85c8ac8ed6ca8023c4c5b5849caaa366e020568a9b97c942340dffaddc6727a0c2334054fb3cf81dce2e87cf4c9a23896b3962ce9fcbb2a32',
     sec: 'eb564e86552f454e4eaad93c9cf19c195c959942e5e0de50bd274afb3f111454'
